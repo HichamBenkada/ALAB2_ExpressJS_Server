6758461219b7426057d736c7f2d1c94eb4b647a4
HichamBENKADA <hicham20014@gmail.com>
2024-01-15 20:10:44 -0500

    EJS view engine is successfully implemented

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 5500;
 
-//----- importing users routes:
-const usersRouter = require("./routes/users");
- 
 /**
- * View engines
+ * Make use of EJS view engine
  */
+app.set("view engine", "ejs");
+// //Styles
+app.use(express.static("styles"));
 
-//---- 1st: -Default Best:::Perscholas...
-const fs = require('fs');
-//----- Tamplate Engines:
-//default
-app.engine("perscholas",(filePath, options, callback)=>{
-    fs.readFile(filePath,(err,content)=>{
-        if(err) return callback(err)
-        const rendered =content
-    .toString()
-    .replaceAll('#title#',`${options.title}`)
-    .replace('#content#',`${options.content}`);
-    return callback(null, rendered)
-    });
-});
+app.get("/", (req, res) => {
+  const options = [
+    {
+      title: "SSR(Server-side rendering)",
+      content:
+        "Server-side rendering is where your site's content is rendered on the web server rather than the browser. This server prepares an HTML file with user-specific data and sends it to the user's machine",
+    },
+    {
+      title: "View Engine!!!",
+      content: `View engines allow us to render web pages using template files. These templates are filled with actual data and served to the client. There are multiple view engines, check out the documentation for the most popular ones to learn how view engine work behined the seen \
+    <a href='https://pugjs.org/api/getting-started.html'>Pug</a>, \
+    <a href='https://www.npmjs.com/package/mustache'>Mustache</a>, or \
+    <a href='https://www.npmjs.com/package/ejs'>EJS</a>. <br/><br/>\
+    In this project, I have created a basic template engine using <code>ejs view engine </code> \
+    , then used <code>res.render</code> to render this page using this content within the template.`,
+    },
+    {
+      title: "EJS (Embedded JavaScript)",
+      content:
+        '"ejs"is a simple templating language that allows developers to generate HTML markup using plain JavaScript. It provides an easy-to-use syntax for embedding dynamic content within HTML templates, making it a popular choice for server-side rendering with Node',
+    },
+  ];
 
-//---- 2st: -Make use of EJS view engine...
-// app.set('view engine', 'ejs');
-
-//---- After middleware we call routes:
-app.use('/users',usersRouter); //redirect requests for users paths to users router
-app.get("/",(req,res)=>{
-    res.send("Helle! Mr. Benkada, You are freaking awesome!...")
+  res.render("index", { title: "Home", options });
 });
 
 // about page
-app.get('/about', (req, res) {
-
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
+// if page not found
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Hicham Server is waiting for clients call on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Hicham's Server is waiting for calls on port ${PORT}`);
 });
